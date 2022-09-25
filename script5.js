3612b15cdb033e4ea658b3d1fbdbfa552901a16a
purwar1997 <shubhampurwar511@gmail.com>
2022-09-25 23:46:44 +0530

    Modified script6.js

--- script5.js
+++ script5.js
@@ -6,17 +6,23 @@
 // otherwise fetch() always returns promise in a resolved state even if the entered API is wrong
 
 const fun = async function () {
-  // await keyword blocks the execution of code inside async function until the promise is resolved
-  const response = await fetch('https://jsonplaceholder.typicode.com/todos/10');
+  try {
+    // await keyword blocks the execution of code inside async function until the promise is resolved
+    const response = await fetch(
+      'https://jsonplaceholder.typicode.com/todos/10'
+    );
 
-  // using status codes
-  // 200 implies data successfully fetched
-  // 404 implies URL not found
-  if (response.status === 200) {
-    const data = await response.json(); // used to fetch data
-    console.log(data);
-  } else {
-    console.log('Unable to fetch data');
+    // using status codes
+    // 200 implies data successfully fetched
+    // 404 implies URL not found
+    if (response.status === 200) {
+      const data = await response.json(); // used to fetch data
+      console.log(data);
+    } else {
+      console.log('Unable to fetch data');
+    }
+  } catch (err) {
+    console.log(`${err.name}: ${err.message}`);
   }
 };
 
