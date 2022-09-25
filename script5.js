22c8b01dcc33af1be06b1adb1c01e0ab8633903f
purwar1997 <shubhampurwar511@gmail.com>
2022-09-25 23:34:52 +0530

    modified script6.js

--- script5.js
+++ script5.js
@@ -3,16 +3,17 @@
 // fetch() returns a promise that can either be in a resolved or rejected state
 // fetch() rejects a promise only when client and server can't communicate with each other i.e
 // either client or server or both are offline
+// otherwise fetch() always returns promise in a resolved state even if the entered API is wrong
 
 const fun = async function () {
-  // await keyword blocks the execution of code inside async function
+  // await keyword blocks the execution of code inside async function until the promise is resolved
   const response = await fetch('https://jsonplaceholder.typicode.com/todos/10');
 
   // using status codes
   // 200 implies data successfully fetched
   // 404 implies URL not found
   if (response.status === 200) {
-    const data = await response.json();
+    const data = await response.json(); // used to fetch data
     console.log(data);
   } else {
     console.log('Unable to fetch data');
